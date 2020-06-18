8fe4335b97e0563cdc1d55724e76911ef6271fa8
ricardo <ricardotvera.54@gmail.com>
2020-06-18 16:26:40 -0700

    started the project and added a few errors

--- bin/main.rb
+++ bin/main.rb
@@ -8,3 +8,6 @@ loop do
   main = Rscan.scan_file(path_file)
   break if !main.file.length.zero? && !main.file[0].length.zero?
 end
+
+main.linter_on
+main.linter_err.each { |key, value| puts "Line #{key}:  #{value}" }
---
+++ lib/rscan.rb
@@ -0,0 +1,81 @@
+class Rscan
+  attr_reader :file, :linter_err
+
+  def initialize(file)
+    @file = file
+    @linter_err = {}
+  end
+
+  def self.scan_file(path_file)
+    Rscan.new(IO.readlines(path_file, chomp: true))
+  rescue Errno::ENOENT
+    puts 'No such file or directory'
+    Rscan.new([''])
+  end
+
+  def linter_on
+    count = 0
+    @file.each do |line|
+      line_length(count, line)
+      withespace_inside_pbb(count, line)
+      withespace_inside_oper(count, line)
+      check_identation(count, line)
+      count = count + 1
+    end
+  end
+
+  private
+
+  def withespace_inside_pbb(count, line)
+    matches1 = ['(', '[', '{']
+    matches2 = [')', ']', '}']
+    num = 0
+    line.split('').each do |char|
+      if matches1.include?(char) && line[num + 1] == ' '
+        @linter_err[count + 1] = 'Whitespace inside after parentheses, brackets or braces'
+        break
+      end
+      if matches2.include?(char) && line[num - 1] == ' '
+        @linter_err[count + 1] = 'Whitespace inside before parentheses, brackets or braces'
+        break
+      end
+      num += 1
+    end
+  end
+
+  def line_length(count, line)
+    @linter_err[count + 1] = "no allowed to have more than 100 characters on a line, you have #{line.length}" if line.length > 100
+  end
+
+  def withespace_inside_oper(count, line)
+    matches1 = ['+', '-', '*', '/', '**']
+    num = 0
+    line.split('').each do |char|
+      if matches1.include?(char) && (line[num + 1] + line[num + 2]) == '  '
+        @linter_err[count + 1] = 'Whitespace inside after an operator'
+        break
+      end
+      if matches1.include?(char) && (line[num - 1] + line[num - 2]) == '  '
+        @linter_err[count + 1] = 'Whitespace inside before an operator'
+        break
+      end
+      num += 1
+    end
+  end
+
+  def empty?(line)
+    return true if line.length.zero?
+
+    false
+  end
+
+  def check_identation(count, line)
+    return if empty?(line)
+    count_space = 0
+    line.split('').each do |char|
+      break if char != ' '
+      count_space += 1
+    end
+    @linter_err[count + 1] = 'Wrong indentation level' if count_space % 2 != 0
+  end
+end

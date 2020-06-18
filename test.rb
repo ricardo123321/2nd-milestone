6d2930d5877b03d2f161943b76cb6c16b0d2aaa2
ricardo <ricardotvera.54@gmail.com>
2020-06-18 16:28:41 -0700

    Create test.rb

---
+++ test.rb
@@ -0,0 +1,6 @@
+print "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tellus ipsum, mollis nec ante vel, ullamcorper egestas nisi. Integer pellentesque luctus ultricies. Nunc non vestibulum magna, id pharetra magna. Fusce faucibus, sem nec finibus tempor, sapien tellus sollicitudin urna, non cursus enim nulla et lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi fermentum eleifend mi, non ultrices dui fermentum at. Etiam fringilla arcu ut tristique hendrerit. In lobortis sapien vitae velit euismod, a ultricies enim suscipit. Cras luctus efficitur lectus, a pulvinar risus malesuada ut. Cras gravida ligula egestas leo dignissim aliquet. Aenean."
+x = [    1, 2]
+y = [1, 2 ]
+sum1 = 1 +  2
+sum2 = 2  + 1 
+ front = "something"

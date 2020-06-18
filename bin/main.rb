8ee7ba378f4efeb18a4a93ac97dd87aead932b4e
ricardo <ricardotvera.54@gmail.com>
2020-06-18 10:54:29 -0700

    started the project

---
+++ bin/main.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require_relative '../lib/rscan.rb'
+main = nil
+
+loop do
+  print 'Enter the path file: '
+  path_file = gets.chomp
+  main = Rscan.scan_file(path_file)
+  break if !main.file.length.zero? && !main.file[0].length.zero?
+end
